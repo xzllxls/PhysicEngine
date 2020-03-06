77505ff8d95b0e5919cdbed0e5842c6250cf592b
DieuMachine <unconfigured@null.spigotmc.org>
2020-03-06 10:36:02 -0500

    Modifier class Force

--- Engine/Force.java
+++ Engine/Force.java
@@ -6,15 +6,27 @@ import Object.Vector3D;
  * @author Linden Brochu
  */
 public class Force {
+
     enum typeForce {
         Gravite, Friction, Propulsion
     }
 
-    Vector3D direction;
+    private Vector3D direction;
 
-    public Force(int newton){
-        //Utiliser cos, sin pour calculer le Vector3D
-
+    /**
+     * Constructeur pour une force null
+     */
+    public Force(){
+        direction = new Vector3D();
     }
+
+    public Vector3D getDirection() {
+        return direction;
+    }
+
+    public void setDirection(Vector3D direction) {
+        this.direction = direction;
+    }
+
 
 }
