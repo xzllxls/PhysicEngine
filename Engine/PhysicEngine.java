bb248998dacd30e57473109e49709653468b442f
DieuMachine <unconfigured@null.spigotmc.org>
2020-03-06 10:37:13 -0500

    Ajouter constante de resistance de l'air

--- Engine/PhysicEngine.java
+++ Engine/PhysicEngine.java
@@ -5,14 +5,5 @@ package Engine;
  */
 public class PhysicEngine {
     public static final double GRAVITY = 1.0;
-
-    /**
-     * Calculer une nouvelle acceleration
-     * @param mass Masse de l'objet
-     * @return Vector3D de l'acceleration
-     */
-    public static Acceleration newAcceleration(int mass, Force[] force){
-        //À Compléter
-        return new Acceleration();
-    }
+    public static final double AIR_RESISTANCE = 0.0;
 }
