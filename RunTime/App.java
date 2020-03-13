4b78871458d95fef2384070d0124eeace0bfda47
DieuMachine <unconfigured@null.spigotmc.org>
2020-03-13 17:09:27 -0400

    Commit

--- RunTime/App.java
+++ RunTime/App.java
@@ -6,7 +6,7 @@ public class App {
      * Appeler plusieurs fois par seconde
      */
     public static void update(){
-
+        //Call your object update function
     }
 
     /**
--- RunTime/RunTimeApp.java
+++ RunTime/RunTimeApp.java
@@ -36,7 +36,8 @@ public class RunTimeApp {
                 if (timeToWait < milisecondPerFrame)
                     TimeUnit.MILLISECONDS.sleep(milisecondPerFrame - timeToWait);
                 else {
-                    System.out.println(ANSI_RED + "Current \"fixed\" frame is to high for computer hardware" + ANSI_RESET);
+                    System.out.println(ANSI_RED + "Current \"fixed\" frame rate is to high for computer hardware" + ANSI_RESET);
+                    System.out.println((timeToWait - milisecondPerFrame) + " miliseconds of latence");
                 }
             } catch (InterruptedException e) {
                 e.printStackTrace();
