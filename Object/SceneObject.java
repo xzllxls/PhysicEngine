c144d1599b202753abfc2dc65fca69f4e55c9370
DieuMachine <unconfigured@null.spigotmc.org>
2020-03-13 19:29:27 -0400

    Ajouter constructeur

--- Object/SceneObject.java
+++ Object/SceneObject.java
@@ -7,9 +7,21 @@ import Engine.Velocity;
  */
 public class SceneObject {
     private int mass;
-    private boolean isRigid;
+    private boolean rigid;
     private Transform transform;
     private Velocity vitesse;
 
-
+    /**
+     * Instancier un SceneObject
+     * @param mass Masse de l'objet
+     * @param rigid Determine si l'objet peut bouger
+     */
+    public SceneObject(int mass, boolean rigid){
+        this.mass = mass;
+        this.rigid = rigid;
+        transform = new Transform(0, 0, 0, 0, 0, 0);
+        if (rigid)
+            vitesse = new Velocity();
+        else vitesse = null;
+    }
 }
