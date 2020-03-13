0e13dcb69cf89d73014f84f039daae030063e035
DieuMachine <unconfigured@null.spigotmc.org>
2020-03-13 17:35:33 -0400

    Ajouter thread pour UI

---
+++ RunTime/AppUi.java
@@ -0,0 +1,16 @@
+package RunTime;
+
+import java.awt.*;
+
+public class AppUi extends Thread{
+    public static Dimension appDim;
+
+    public AppUi(/*Canvas*/ int width, int height){
+        appDim = new Dimension(width, height);
+    }
+
+    public void run(){
+        
+    }
+
+}
